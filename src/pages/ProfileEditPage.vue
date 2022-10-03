b12e3fe55ca806abb099151aaf59eae3df8b9856
Vlad Brok <vlad.brok99@gmail.com>
2022-10-03 21:55:12 +0300

    Use a dialog plugin

--- src/pages/ProfileEditPage.vue
+++ src/pages/ProfileEditPage.vue
@@ -3,7 +3,7 @@
   <div class="q-mx-auto" style="max-width: 18rem">
     <main>
       <section class="row q-gutter-md items-center justify-between">
-        <q-btn label="Отмена" color="negative" @click="onCancel" />
+        <q-btn label="Удалить" color="negative" @click="onDelete" />
         <q-btn label="Сохранить" color="primary" @click="onSave" />
       </section>
     </main>
@@ -16,8 +16,18 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'ProfileEditPage',
   methods: {
-    onCancel() {
-      this.$router.push('/profile');
+    onDelete() {
+      this.$q
+        .dialog({
+          title: 'Подтверждение',
+          message: 'Вы уверены, что хотите удалить информацию о себе?',
+          cancel: 'Отмена',
+          ok: 'ОК',
+          persistent: true,
+        })
+        .onOk(() => {
+          this.$router.push('/profile');
+        });
     },
     onSave() {
       this.$router.push('/profile');
