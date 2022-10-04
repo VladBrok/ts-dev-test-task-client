6026c6f0860b6f05843f3d08c6572f281483929b
Vlad Brok <vlad.brok99@gmail.com>
2022-10-04 22:41:19 +0300

    Create a CustomSuspense component

---
+++ src/components/CustomSuspense.vue
@@ -0,0 +1,20 @@
+<template>
+  <Suspense>
+    <div class="q-mx-auto" style="max-width: 30rem">
+      <slot />
+    </div>
+    <template #fallback>
+      <div class="fullscreen row justify-center items-center">
+        <q-spinner color="primary" size="5em" :thickness="3" />
+      </div>
+    </template>
+  </Suspense>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CustomSuspense',
+});
+</script>
