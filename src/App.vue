6756678065a1e18917e7c290e864e0eeb573c862
VladBrok <vlad.brok99@gmail.com>
2022-10-09 12:41:54 +0300

    Add a global error handler

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Notify } from 'quasar';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  setup() {
+    window.onunhandledrejection = (e) => {
+      console.log(e.reason);
+      Notify.create({
+        type: 'negative',
+        message:
+          'Не удалось подключиться к серверу. Пожалуйста, повторите попытку позже',
+      });
+    };
+  },
 });
 </script>
